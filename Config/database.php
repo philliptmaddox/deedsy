1495615dc58159aa7203dd78edc2ac75bec8f2e0
jeremy.shearer <mohicanlove@gmail.com>
2012-04-21 10:04:06 -0500

    crud for users

---
+++ Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'deedsy',
+		'password' => 'D33dsy',
+		'database' => 'flash_deedsy',
+	);
+}
