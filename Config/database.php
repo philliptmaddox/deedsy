8a1a84dcf1ea9fef5764b76725d4627cd63fb1af
Phillip Maddox <philliptmaddox@gmail.com>
2012-05-30 20:01:38 -0500

    initial

--- Config/database.php
+++ Config/database.php
@@ -74,9 +74,9 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'flash_deedsy',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
