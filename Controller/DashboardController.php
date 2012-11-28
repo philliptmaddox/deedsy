674153b6068f6fd7d9f17a23f4399bc980bb04c3
Daniel Gibbs <danielgibbs@danielgibbs.us>
2012-11-27 20:15:05 -0600

    removed empty beforeFilter method

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -3,9 +3,6 @@ class DashboardController extends AppController {
 	var $uses = array('User', 'Deed', 'Tag');
 	public $helpers = array('Html', 'Form', 'Time');	
 	
-	public function beforeFilter() {
-	}
-	
 	public function index() {
 		if ($this->Auth->user('id') != null) {
 			$id = $this->Auth->user('id');
